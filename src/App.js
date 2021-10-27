f586217a2e03fdb388c8dde5ec5db6b0ddb79ca4
niki <nikolayp1707@gmail.com>
2021-10-27 10:59:12 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
+import React from 'react';
+import Tags from "./Tags";
 import "./App.css";
 
 function App() {
+  const [tags] = React.useState(["boomdotdev", "task", "tags", "react"])
   return (
-    <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
-      </div>
+    <div className="Main">
+      <Tags tags={tags}/>
     </div>
   );
 }
